05066e975b777eb0649cd3e6c222dad87685c62a
ateistemes <timur.tynychbekov@gmail.com>
2023-10-18 18:01:44 +0600

    T

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -2,6 +2,28 @@ package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
+        int i, j, k;
+        int n = height;
+        for (i = 1; i <= n; i++) {
+            for (j = 1; j < i; j++) {
+                System.out.print(' ');
+            }
 
+            for (k = i; k <= n; k++) {
+                System.out.print("8 ");
+            }
+            System.out.println();
+        }
+        for (i = n - 1; i >= 1; i--) {
+            for (j = 1; j < i; j++) {
+                System.out.print(' ');
+            }
+            for (k = i; k <= n; k++) {
+                System.out.print("8 ");
+            }
+
+            System.out.println();
+        }
     }
+
 }
